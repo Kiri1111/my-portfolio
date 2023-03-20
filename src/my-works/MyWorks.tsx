9a6d5dd3aa33dd5fe8f7f985ff98df6868d4f7c7
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2023-03-20 09:39:19 +0300

    feat: add mywork

--- src/my-works/MyWorks.tsx
+++ src/my-works/MyWorks.tsx
@@ -5,6 +5,7 @@ import {MyWork} from './my-work/MyWork'
 import {Title} from "../common/components/title/Title";
 import socialImg from '../assets/images/social-networking.webp'
 import todoImg from '../assets/images/todolist.jpg'
+import cardsImg from '../assets/images/learn-by-cards.webp'
 import Fade from "react-awesome-reveal";
 
 export const MyWorks = () => {
@@ -13,6 +14,9 @@ export const MyWorks = () => {
     }
     const todo = {
         backgroundImage: `url(${todoImg})`
+    }
+    const cards={
+        backgroundImage:`url(${cardsImg})`
     }
     return (
         <div className={s.myWorksBlock}>
@@ -24,6 +28,9 @@ export const MyWorks = () => {
                         <MyWork style={todo}
                                 title={'To Do List'}
                                 description={'Create todolist. It is a list of things you need to do or want to do. Traditionally, they are written on a piece of paper and organized in order of priority. When a task is completed, it is usually crossed off the list.'}/>
+                        <MyWork style={cards}
+                                title={'Card training'}
+                                description={'Сards are a way to memorize information. Picture in electronic form with two working sides. On one is a word, a definition, a term, or some kind of phenomenon. On the other - an explanation to it, a translation or a brief essence. '}/>
                         <MyWork style={social}
                                 title={'Social network'}
                                 description={'Find your friends. This is an Internet platform, a site that allows users registered on it to post information about themselves and communicate with each other, establishing social connections. '}/>
