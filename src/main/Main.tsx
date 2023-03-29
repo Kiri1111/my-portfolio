f70809169f50de1dd6f5c04f4dadcf0837db62c0
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2023-03-29 18:58:35 +0300

    feat(main): icon links

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -5,16 +5,37 @@ import mainImage from "../assets/images/main.jpg";
 import {ParticleComponent} from "../particle/Particle";
 import Fade from "react-awesome-reveal";
 import ReactTypingEffect from "react-typing-effect";
+import gitHubI from "../assets/icons/github.png";
+import linkedinI from "../assets/icons/linkedin.png";
+import telegramI from "../assets/icons/telegram.png";
 
 export const Main = () => {
     const mainBackground = {
         backgroundImage: `url(${mainImage})`
     }
-
+    const gitHubIcon = gitHubI
+    const linkedinIcon = linkedinI
+    const telegramIcon = telegramI
     return (
         <div id={'Main'} className={s.mainBlock} style={mainBackground}>
             <ParticleComponent/>
-
+            <div className={s.divTel}>
+                <div>
+                    <a href={'https://github.com/Kiri1111'}>
+                        <img alt={'icon'} className={s.icon} src={gitHubIcon}/>
+                    </a>
+                </div>
+                <div>
+                    <a href={'https://t.me/MedvedevKirill92'}>
+                        <img alt={'icon'} className={s.icon} src={telegramIcon}/>
+                    </a>
+                </div>
+                <div>
+                    <a href={'https://www.linkedin.com/in/kirill-medvedev-a75893253/'}>
+                        <img alt={'icon'} className={s.icon} src={linkedinIcon}/>
+                    </a>
+                </div>
+            </div>
             <div className={styleContainer.container}>
                 <div className={s.text}>
                     <Fade>
@@ -30,6 +51,7 @@ export const Main = () => {
                     </Fade>
                 </div>
             </div>
+
         </div>
     );
 };
